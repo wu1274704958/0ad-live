58074fcc835ee2313f6ea362e8c601d3792aa437
eqd <1274704958@qq.com>
2026-04-10 15:28:57 +0800

    Add HOSTTYPE environment variable setup for amd64 in update-workspaces.bat

--- build/workspaces/update-workspaces.bat
+++ build/workspaces/update-workspaces.bat
@@ -1,6 +1,11 @@
 @echo off
 rem ** Create Visual Studio Workspaces on Windows **
 
+rem Check if --amd64 is passed, and set HOSTTYPE environment variable
+for %%A in (%*) do (
+    if "%%A"=="--amd64" set HOSTTYPE=amd64
+)
+
 cd /D "%~dp0"
 cd ..\bin
 if not exist ..\workspaces\vs2022\SKIP_PREMAKE_HERE premake5.exe --file="../premake/premake5.lua" --outpath="../workspaces/vs2022" %* vs2022 || exit /b 1
