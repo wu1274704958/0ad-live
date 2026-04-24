0f130d1c615c6b8dbd0bf8b87fda8a2d0149b48a
eqd <1274704958@qq.com>
2026-04-24 18:52:18 +0800

    Fix stochastic distribution parameter update to only apply when stochastic is enabled

--- source/simulation2/components/CCmpDecay.cpp
+++ source/simulation2/components/CCmpDecay.cpp
@@ -125,9 +125,12 @@ public:
 
 		m_Stochastic = m_SinkProb < 1.0f;
 
-		std::negative_binomial_distribution<int>::param_type new_params(
-			6, Clamp(m_SinkProb, 1e-3f, 1.0f));
-		m_Distribution.param(new_params);
+		if (m_Stochastic)
+		{
+			std::negative_binomial_distribution<int>::param_type new_params(
+				6, Clamp(m_SinkProb, 1e-3f, 1.0f));
+			m_Distribution.param(new_params);
+		}
 
 		// Detect unsafe misconfiguration
 		if (m_Active && !ENTITY_IS_LOCAL(GetEntityId()))
